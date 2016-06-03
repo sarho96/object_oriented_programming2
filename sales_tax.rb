61807b993bc9e8449588d30ac2339bee62608381
Stella <stellaha4@gmail.com>
2016-06-03 19:24:17 -0400

    created tax_total method to determine sales tax & import tax

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,35 +1,43 @@
+################ Items Class ################
+
 class Items
   attr_accessor :name, :price, :sales_tax, :import_tax
 
-  def initialize (name, price, sales_tax, import_tax)
+  def initialize(name, price, sales_tax, import_tax)
     @name = name
-    @price = price
+    @price = price.to_f
     @sales_tax = sales_tax
     @import_tax = import_tax
   end
 
-  def s_tax
+#### Tax Calculation (True or False) ####
+
+def tax_total
     if @sales_tax == "true"
       @price * 1.10
     elsif @sales_tax == "false"
       @price
-    end
-  end
-
-  def i_tax
-    if @import_tax == "true"
+    elsif @import_tax == "true"
       @price * 1.05
     elsif @import_tax == "false"
       @price
+    elsif (@import_tax == "true") && (@sales_tax == "true")
+      @price * (1.10 + 1.05)
+    elsif (@import_tax == "false") && (@sales_tax == "false")
+      @price
     end
-  end
+end
+
 
 end
+
+################ Receipt Class ################
 
 class Receipt
 
 end
 
+################ Instructions ################
 
-chocolate = Items.new("chocolate", 12.99, "true", "false")
-puts chocolate.s_tax
+chocolate = Items.new("chocolate", 4.20, "false", "false")
+puts chocolate.tax_total
