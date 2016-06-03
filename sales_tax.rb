cf06a05f36729726eb6adabf450234f081cf539f
Stella <stellaha4@gmail.com>
2016-06-03 18:07:02 -0400

    created 2 methods to calculate sales tax and methods True and False

--- sales_tax.rb
+++ sales_tax.rb
@@ -1,27 +1,35 @@
-=begin
-basic sales tax = 10%
-all goods except:
-    - books
-    - food
-    - medical products
+class Items
+  attr_accessor :name, :price, :sales_tax, :import_tax
 
-import duty = 5%
-no exceptions
+  def initialize (name, price, sales_tax, import_tax)
+    @name = name
+    @price = price
+    @sales_tax = sales_tax
+    @import_tax = import_tax
+  end
 
-always receive a receipt that lists all items and price (plus tax)
-receipt finishes with total cost of items
-receipt finishes with total amouts of sales tax paid
+  def s_tax
+    if @sales_tax == "true"
+      @price * 1.10
+    elsif @sales_tax == "false"
+      @price
+    end
+  end
 
-rounding rules for sales tax:
-
-sales tax = (tax_rate_percent * shelf_price / 100).round(0.05)
-
-=end
-
-class Item
+  def i_tax
+    if @import_tax == "true"
+      @price * 1.05
+    elsif @import_tax == "false"
+      @price
+    end
+  end
 
 end
 
 class Receipt
 
 end
+
+
+chocolate = Items.new("chocolate", 12.99, "true", "false")
+puts chocolate.s_tax
